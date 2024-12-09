e72352f11925ab094a0bcb56075ab359b640c80e
Mercy Ndirangu <ndirangumercy4@gmail.com>
2024-12-09 07:51:11 -0800

    creating a new user

---
+++ controllers/UsersController.js
@@ -0,0 +1,32 @@
+import crypto from 'crypto';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const usersCollection = dbClient.db.collection('users');
+
+    const existing = await usersCollection.findOne({ email });
+    if (existing) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPwd = crypto.createHash('sha1').update(password).digest('hex');
+
+    const result = await usersCollection.insertOne({ email, password: hashedPwd });
+
+    const newUser = { id: result.insertedId, email };
+
+    return res.status(201).json(newUser);
+  }
+}
+
+export default UsersController;
