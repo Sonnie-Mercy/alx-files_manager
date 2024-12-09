1f1c72fd686adca01e66ac3d8556ece28a2504fe
Mercy Ndirangu <ndirangumercy4@gmail.com>
2024-12-09 05:47:15 -0800

    redis

--- utils/redis.js
+++ utils/redis.js
@@ -1,29 +1,41 @@
 import { createClient } from 'redis';
-import { promisify } from 'util';
 
 class RedisClient {
   constructor () {
     this.client = createClient();
+
     this.client.on('error', (err) => console.error('Redis Client Error:', err));
-    this.getAsync = promisify(this.client.get).bind(this.client);
+
+    this.client.connect().catch((err) => console.error('Redis Connection Error:', err));
   }
 
   isAlive () {
-    return this.client.connected;
+    return this.client.isReady;
   }
 
   async get (key) {
-    return this.getAsync(key);
+    try {
+      return await this.client.get(key);
+    } catch (err) {
+      console.error(`Error getting key ${key}:`, err);
+      return null;
+    }
   }
 
   async set (key, value, duration) {
-    const setAsync = promisify(this.client.setex).bind(this.client);
-    return setAsync(key, duration, value);
+    try {
+      await this.client.set(key, value, { EX: duration });
+    } catch (err) {
+      console.error(`Error setting key ${key} with value ${value}:`, err);
+    }
   }
 
   async del (key) {
-    const delAsync = promisify(this.client.del).bind(this.client);
-    return delAsync(key);
+    try {
+      await this.client.del(key);
+    } catch (err) {
+      console.error(`Error deleting key ${key}:`, err);
+    }
   }
 }
 
